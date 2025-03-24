545bd0309a1dc135fbf34ba2ea5ed113128fb836
ljunn9 <147462254+ljunn9@users.noreply.github.com>
2025-03-23 17:26:46 -0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -12,12 +12,4 @@ def extract_fft(trial, fs=250):
 def generate_reference_signals(freqs, t):
     return [np.array([np.sin(2*np.pi*f*t), np.cos(2*np.pi*f*t)]).T for f in freqs]
 
-def run_cca(trial, ref_signals):
-    cca = CCA(n_components=1)
-    trial_data = trial.T
-    corrs = []
-    for ref in ref_signals:
-        X_c, Y_c = cca.fit_transform(trial_data, ref)
-        corr = np.corrcoef(X_c.T[0], Y_c.T[0])[0, 1]
-        corrs.append(corr)
-    return np.argmax(corrs)
+
